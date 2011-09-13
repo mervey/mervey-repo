1804b007edda33e6d6771afe3a1f24a45f4fe388
merve <merveyuzbasioglu89@gmail.com>
2011-09-14 01:40:38 +0300

    verilen bytearray içerisinde utf8'e ait olmayan byte'ları çıkarır.

---
+++ non-utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+s=input("Bir şeyler giriniz: ")
+print s.decode('utf-8', 'ignore')
+
+# soruyu yanlış anlamam ve yanlış şeylere bakmam sebebiyle 1.5-2 saat sürdü:(
+# tam olarak örnek girdide yazıldığı gibi girildiğinde istenilen sonucu alıyoruz.
+
